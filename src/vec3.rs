1073190670c6cd4cbfa9ad871e627507b904b385
juancoquet <juancoquet@gmail.com>
2024-06-15 12:23:11 +0100

    divassign f32 for vec3

--- src/vec3.rs
+++ src/vec3.rs
@@ -60,6 +60,17 @@ impl std::ops::MulAssign<f32> for Vec3 {
     }
 }
 
+impl std::ops::DivAssign<f32> for Vec3 {
+    fn div_assign(&mut self, other: f32) {
+        // use the reciprocal of the divisor to avoid division as division is more expensive than
+        // multiplication
+        let reciprocal = 1.0 / other;
+        self.v[0] *= reciprocal;
+        self.v[1] *= reciprocal;
+        self.v[2] *= reciprocal;
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -121,6 +132,19 @@ mod tests {
             v: [a * x, b * x, c * x],
         };
         v *= x;
+
+        assert_eq!(exp, v);
+    }
+
+    #[test]
+    fn test_div_assign() {
+        let (a, b, c) = (4.0, 5.0, 6.0);
+        let x = 2.0;
+        let mut v = Vec3 { v: [a, b, c] };
+        let exp = Vec3 {
+            v: [a / x, b / x, c / x],
+        };
+        v /= x;
 
         assert_eq!(exp, v);
     }

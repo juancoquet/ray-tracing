dceb59f5856f3b531f9a6f7305b7f975e48f6c69
juancoquet <juancoquet@gmail.com>
2024-06-23 20:43:03 +0100

    update hittables

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ use camera::Camera;
 use color::Color;
 use hittable::Hittable;
 use materials::lambertian::Lambertian;
+use materials::metal::Metal;
 use point::Point;
 use sphere::Sphere;
 use std::fs::File;
@@ -23,12 +24,14 @@ fn main() {
 
     let material_floor = Lambertian::new(Color::new(0.8, 0.8, 0.8));
     let material_centre = Lambertian::new(Color::new(0.1, 0.2, 0.5));
-    let material_left = Lambertian::new(Color::new(0.8, 0.8, 0.8));
-    let material_right = Lambertian::new(Color::new(0.8, 0.6, 0.2));
+    let material_left = Metal::new(Color::new(0.8, 0.8, 0.8));
+    let material_right = Metal::new(Color::new(0.8, 0.6, 0.2));
 
-    let sph1 = Sphere::new(Point::new(0.0, 0.0, -1.0), 0.5);
-    let sph2 = Sphere::new(Point::new(0.0, -100.5, -1.0), 100.0);
-    let hittables: [&dyn Hittable; 2] = [&sph1, &sph2];
+    let floor = Sphere::new(Point::new(0.0, -100.5, -1.0), 100.0, &material_floor);
+    let centre = Sphere::new(Point::new(0.0, 0.0, -1.0), 0.5, &material_centre);
+    let left = Sphere::new(Point::new(0.0, -100.5, -1.0), 100.0, &material_left);
+    let right = Sphere::new(Point::new(0.0, -100.5, -1.0), 100.0, &material_right);
+    let hittables: [&dyn Hittable; 4] = [&floor, &centre, &left, &right];
 
     let reflection_depth = 50;
     let ppm = camera.render(&hittables, reflection_depth);

2262006593ad675b4f0b171436526bfc40799546
juancoquet <juancoquet@gmail.com>
2024-06-23 21:32:25 +0100

    fuzziness for metals

--- src/main.rs
+++ src/main.rs
@@ -24,8 +24,8 @@ fn main() {
 
     let material_floor = Lambertian::new(Color::new(0.8, 0.8, 0.0));
     let material_centre = Lambertian::new(Color::new(0.1, 0.2, 0.5));
-    let material_left = Metal::new(Color::new(0.8, 0.8, 0.8));
-    let material_right = Metal::new(Color::new(0.8, 0.6, 0.2));
+    let material_left = Metal::new(Color::new(0.8, 0.8, 0.8), 0.3);
+    let material_right = Metal::new(Color::new(0.8, 0.6, 0.2), 1.0);
 
     let floor = Sphere::new(Point::new(0.0, -100.5, -1.0), 100.0, &material_floor);
     let centre = Sphere::new(Point::new(0.0, 0.0, -1.2), 0.5, &material_centre);
--- src/materials/metal.rs
+++ src/materials/metal.rs
@@ -1,22 +1,31 @@
-use crate::{color::Color, hit_record::HitRecord, point::Point, ray::Ray};
+use crate::{
+    color::Color, hit_record::HitRecord, interval::Interval, point::Point, ray::Ray, vec3::Vec3,
+};
 
 use super::material::Material;
 
 pub struct Metal {
     albedo: Color,
+    fuzz: f64,
 }
 
 impl Metal {
-    pub fn new(albedo: Color) -> Self {
-        Metal { albedo }
+    pub fn new(albedo: Color, fuzz: f64) -> Self {
+        let fuzz_norm = Interval::new(0.0, 1.0).clamp(fuzz);
+        Metal {
+            albedo,
+            fuzz: fuzz_norm,
+        }
     }
 }
 
 impl Material for Metal {
     fn scatter(&self, incident: &Ray, hit: &HitRecord) -> (Ray, Color) {
         let reflection_direction = incident.direction.reflect(&hit.normal);
+        let reflection_direction_fuzzy =
+            reflection_direction.unit() + (Vec3::random_unit() * self.fuzz);
         let reflection_origin = Point::new(hit.point.x(), hit.point.y(), hit.point.z());
-        let reflected_ray = Ray::new(reflection_origin, reflection_direction);
+        let reflected_ray = Ray::new(reflection_origin, reflection_direction_fuzzy);
         let color = Color::new(self.albedo.x(), self.albedo.y(), self.albedo.z());
         (reflected_ray, color)
     }
